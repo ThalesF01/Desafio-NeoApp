13c5cd2472dfcc38df63eb37e9d53cd7767a4ce9
Thales <thalesgabriel07@gmail.com>
2023-03-21 22:43:41 -0300

    conexao com api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import md5 from 'md5'
+
+const publicKey = 'a94e39fa014ec8b45c47f2358d0ace06';
+const privateKey = '15774ea4943d696d870af8aa0628c0bd6f6af674';
+const timestamp = '1679439171095';
+const time = Number(new Date())
+
+
+const hash = md5(time + privateKey + publicKey);
 
 function App() {
+
+  useEffect(() => {
+    axios.get(`https://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`)
+  .then(response => console.log(time,response))
+  .catch(err => console.error(err));
+  }, []);
+
   return (
-    <h1>Teste</h1>
+      <div className='marvel'>
+          <h1>Teste</h1>
+      </div>
   );
 }
 
