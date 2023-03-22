47f1b914aaefee363df66b1d35d76cc256fdde1a
Thales <thalesgabriel07@gmail.com>
2023-03-22 01:53:50 -0300

    colocando primeiras imagens

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../Services/api'
+import { Container } from './sytles'
 
 const Characters = () =>{
     const [characters, setCharacters] = useState([])
@@ -15,7 +16,26 @@ const Characters = () =>{
         .catch(err => console.log(err))
     }, [])
 
-    return <h1>Characters: </h1>
+    return(
+        <Container>
+            <h1>Characters</h1>
+            <ul>
+                {characters.map(character =>{
+                    return(
+                        <li key={character.id}>
+                            <img 
+                            src={`${character.thumbnail.path}.${character.thumbnail.extension}`} 
+                            alt={`Foto do ${character.name}`} />
+                            <span className="name">
+                                {character.name}
+                            </span>
+                        </li>
+                    )
+                })}
+            </ul>
+        </Container>
+    )
+    
 }
 
 export default Characters
