80e638c37a3067e58e3283b2ec72341b6d93d983
Thales <thalesgabriel07@gmail.com>
2023-03-24 15:23:33 -0300

    Tentativa subir producao

--- src/Services/api.jsx
+++ src/Services/api.jsx
@@ -6,7 +6,7 @@ const privateKey = 'babd4db99c02ee9a5d1bd653484e3c5a6a29da7b';
 const time = Number(new Date())
 const hash = md5(time + privateKey + publicKey);
 
-const api = axios.create({
+export const api = axios.create({
     baseURL: `https://gateway.marvel.com/v1/public/`,
     params: {
         ts: time,
