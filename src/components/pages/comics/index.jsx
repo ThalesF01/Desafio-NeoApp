f6760ce07aa7d638ff2892343c9b1e3991bade36
Thales <thalesgabriel07@gmail.com>
2023-03-24 15:41:19 -0300

    Tentativa subir producao 3

--- src/components/pages/comics/index.jsx
+++ src/components/pages/comics/index.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react'
-import api from '../../../services/api'
+
 import Header from '../../main/header'
 import { useNavigate } from 'react-router-dom'
 import {  Div, Container, ContainerHq, Title, Img } from '../../../styles/list';
 import axios from 'axios';
+import md5 from 'md5';
 
 const Comics = () =>{
+
+    const publicKey = 'c35bd6ac7d24ffa9ab6b91748102fc41';
+  const privateKey = 'babd4db99c02ee9a5d1bd653484e3c5a6a29da7b';
+  const time = Number(new Date())
+  const hash = md5(time + privateKey + publicKey);
+  
 
     const [comics, setComics] = useState([])   
 
     useEffect(()=>{
         axios
-        .get('https://gateway.marvel.com/v1/public/comics')
+        .get(`https://gateway.marvel.com/v1/public/comics?ts=${time}&apikey=${publicKey}&hash=${hash}`)
         .then(response =>{
             console.log(response.data.data.results)
             setComics(response.data.data.results)           
